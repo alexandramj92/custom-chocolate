66d01084fcd33786730c72f6cbcfa721721f3720
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-06-24 15:30:26 -0700

    changed config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://react-image-upload.herokuapp.com'
+  ? 'https://custom-chocolate-client.herokuapp.com/'
   : 'http://localhost:8080'
--- server/config.js
+++ server/config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://react-image-upload.surge.sh'
+  ? 'https://custom-chocolate-server.herokuapp.com/'
   : 'http://localhost:3000'
