5cdd03c57e186baa7f6f0fb690b758ccd7c010de
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-06-24 13:36:14 -0700

    made some changes

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const cloudinary = require('cloudinary')
 const formData = require('express-form-data')
 const cors = require('cors')
 const { CLIENT_ORIGIN } = require('./config')
 
-
-// const express = require('express');
-const favicon = require('express-favicon');
-const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-// app.listen(port);
 
 const app = express()
 
@@ -49,5 +33,13 @@ app.post('/image-upload', (req, res) => {
     .then(results => res.json(results))
     .catch((err) => res.status(400).json(err))
 })
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => console.log('👍'))
