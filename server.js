44f629f6a6d673eb4000f2f4f01e96d134e0eaa2
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-06-24 19:27:55 -0700

    trying something

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express')
 const path = require('path')
 const cloudinary = require('cloudinary')
 const formData = require('express-form-data')
-// const cors = require('cors')
-// const { CLIENT_ORIGIN } = require('./config')
+const cors = require('cors')
+const { CLIENT_ORIGIN } = require('./config')
 
 
 const app = express()
 app.use(formData.parse())
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 cloudinary.config({ 
@@ -20,9 +19,9 @@ cloudinary.config({
 
 
   
-// app.use(cors({ 
-//   origin: CLIENT_ORIGIN 
-// })) 
+app.use(cors({ 
+  origin: CLIENT_ORIGIN 
+})) 
 
 
 app.get('/wake-up', (req, res) => res.send('👌'))
@@ -40,9 +39,7 @@ app.post('/image-upload', (req, res) => {
 
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
