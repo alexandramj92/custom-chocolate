a2b404fef2202ce099d1ffa18738fe7b27e1f9d3
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-06-25 09:05:24 -0700

    change

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://custom-chocolate.herokuapp.com/'
+  ? 'https://custom-chocolate.herokuapp.com'
   : 'http://localhost:3000'
