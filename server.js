05b1e95755898a722b0c42a9869f4c3a4922a885
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-06-24 20:04:13 -0700

    another test

--- server.js
+++ server.js
@@ -43,10 +43,15 @@ app.post('/image-upload', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.use(express.static(__dirname));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  pp.get('/ping', function (req, res) {
+    return res.send('pong');
+   });
+   app.get('/*', function (req, res) {
+     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+   });
+  
 }
 
 
