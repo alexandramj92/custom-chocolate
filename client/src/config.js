f80ebac26cb1af7aac9e54101f87f12422994cad
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-07-09 10:43:13 -0700

    removed dash from link

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://chelsea-chocolate-factory.herokuapp.com/'
+  ? 'https://chelsea-chocolate-factory.herokuapp.com'
   : 'http://localhost:8080'
