92a2c71eb0e1da9506f30aaffb03de55de82bd3e
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-07-09 10:31:14 -0700

    changed link in config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://custom-chocolate.herokuapp.com'
+  ? 'https://chelsea-chocolate-factory.herokuapp.com/'
   : 'http://localhost:3000'
